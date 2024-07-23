93d78321694cee34ff216afa9aabba8e0139dfa8
ayan4m1 <andrew@bulletlogic.com>
2024-07-23 00:25:11 -0400

    WIP theme update script, currently does not find all zip files in theme directory correctly because busybox sucks

---
+++ init/MUOS/task/Update Themes.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ ! -f /opt/muos/script/var/func.sh ]; then
+  echo "Anbernic scripts were not found! Looks like you're running on your desktop... Exiting!"
+  exit 1
+fi
+
+. /opt/muos/script/var/func.sh
+
+. /opt/muos/script/var/device/storage.sh
+
+if [ ! -d "${DC_STO_ROM_MOUNT}/MUOS/theme" ]; then
+  echo "Theme directory was not found! Looks like you're running on your desktop... Exiting!"
+  exit 1
+fi
+
+pkill -STOP muxtask
+
+WIFI_CONNECTED=$(ping -c 1 github.com | grep "bytes from" | wc -l)
+
+if [ "$WIFI_CONNECTED" = "0" ]; then
+  echo "You do not appear to be connected to the internet. Exiting!"
+  exit 1
+fi
+
+THEME_FILES=""
+while IFS= read -r FILE; do
+THEME_FILES="$THEME_FILES \"$FILE\""
+done <"${DC_STO_ROM_MOUNT}/MUOS/theme"
+
+for FILE in "$THEME_FILES"; do
+  THEME_FILENAME=$(basename -s ".zip" "$FILE")
+  THEME_NAME=$(echo "$THEME_FILENAME" | sed -r 's/\./ /g')
+
+  echo "Processing theme ${THEME_NAME}"
+
+  THEME_INDEX=$(curl -o /dev/null -s -w "%{http_code}" "https://github.com/MustardOS/theme/tree/main/${THEME_NAME}")
+
+  if [ "$THEME_INDEX" = "404" ]; then
+    echo "Skipping theme ${THEME_NAME} because it was not found in the theme repository. Check the filename: ${THEME_FILENAME}"
+    continue
+  fi
+
+  echo "Updating ${THEME_NAME} to latest version..."
+  curl -o "/tmp/${FILE}" "https://github.com/MustardOS/theme/releases/latest/download/${FILE}"
+
+  echo "Running extract script on newly downloaded theme..."
+  /bin/sh "${DC_STO_ROM_MOUNT}/MUOS/script/mux/extract.sh" "/tmp/${FILE}"
+
+  rm "/tmp/${FILE}"
+done
+
+echo "Completed updating themes!"
+sleep 2
+pkill -CONT muxtask
+exit 0
